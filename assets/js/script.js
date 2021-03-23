a51accda195947fba223b346b830889bd4f70487
Peter Nguyen <p3t3rn9uy3n@gmail.com>
2021-03-22 19:44:25 -0500

    added date picker js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
+// BMI CALCULATOR
 function calcBMI() {
   var weight = document.bmiform.pounds.value, height = document.bmiform.inches.value;
   document.bmiform.bmi.value = parseInt((weight * 703) / (height * height));
 } 
+
+// DATE PICKER
+const calender = document.querySelector('.datepicker');
+M.Datepicker.init(calender,{
+  Format:'dd-mmmm-yyyy'
+});
+      
+
+$('.dropdown-trigger').dropdown();
+
+M.AutoInit();
